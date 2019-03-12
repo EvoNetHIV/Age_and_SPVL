91b644e812a73b9ea72225f625ec0f7338efbcdc
Steven M. Goodreau <goodreau@uw.edu>
2019-03-12 09:26:30 -0700

    additions to the first hetero script.  still in progress though!

--- experiments/ageSPVL_het01.R
+++ experiments/ageSPVL_het01.R
@@ -1,18 +1,23 @@
 
+
+devtools::install_github( "statnet/tergmLite",force=T)
+devtools::install_github( "statnet/EpiModel", ref ="fast_edgelist",force=T)
+
+
 library(evonet)
 #options(error=recover) # go into debug mode on error
 #--------------------------------------------------------------
 
 # items used in calcuation of params
-initial_pop      = 100
-initial_infected = 10
-mean_sqrt_age_diff = 1.2              # CHECK
-meandeg = 0.7                         # CHECK
+initial_pop           = 100
+initial_infected      = 10
+mean_sqrt_age_diff    = 1.2             # CHECK value
+meandeg               = 0.7             # CHECK value
 
 param_list <- list(
   
   # Top-level simulation parameters
-  model_name = "h01",
+  model_name       = "h01",
   n_steps          = 365,
   nsims            = 5,
   ncores           = 5,
@@ -27,48 +32,48 @@ param_list <- list(
   initial_infected = initial_infected,
   
   # Demographic parameters
-  model_sex = "hetero",
-  asmr_data_male = "south_africa_male",
-  asmr_data_female = "south_africa_female",
-  initial_agedata_male = "stable_age_no_hiv_dist",
-  initial_agedata_female = "stable_age_no_hiv_dist",
-  birth_model = "exponential_growth",
-  baseline_input_exp_growth = 0.007*(initial_pop/100), # Calibrated by hand; assumes initial_agedata_male = "stable_age_no_hiv_dist"
-  pop_growth_rate_annual   = 0.01,
-  min_age = 16,
-  max_age = 99,
+  model_sex                     = "hetero",
+  asmr_data_male                = "south_africa_male",
+  asmr_data_female              = "south_africa_female",
+  initial_agedata_male          = "stable_age_no_hiv_dist",
+  initial_agedata_female        = "stable_age_no_hiv_dist",
+  birth_model                   = "exponential_growth",
+  baseline_input_exp_growth     = 0.007*(initial_pop/100), # Calibrated by hand; assumes initial_agedata_male = "stable_age_no_hiv_dist"
+  pop_growth_rate_annual        = 0.01,
+  min_age                       = 16,
+  max_age                       = 99,
 
   # Factors affecting transmission probabilities (note that some may be overwritten by calling program)
-  trans_lambda  =  0.000247 * 15, # some value times Hughes et al. (b/c they may be biased)  
-  trans_RR_receptive_vaginal = 1.5,
+  trans_lambda                  =  0.000247 * 15, # CHECK some value times Hughes et al. (b/c they may be biased)  
+  trans_RR_receptive_vaginal    = 1.5, # CHECK
 
   # Condom use 
-  age_condom_use_halves = 35,
-  condom_use_age = TRUE,
-  condom_prob <- 0.80, # When condom_use_age == TRUE this gives probability that 16-year old uses condom, otherwise this is the probability for everyone
-  condom_use_rel_dur = FALSE,
-  RR_cond_male_concurrent  = 1.0,
-  RR_cond_fem_concurrent   = 1.0,
+  condom_use_age                = TRUE,
+  age_condom_use_halves         = 35,
+  condom_prob                   = 0.80, # When condom_use_age == TRUE, this = prob 16-yo uses condom, otherwise prob for all
+  condom_use_rel_dur            = FALSE,
+  RR_cond_male_concurrent       = 1.0,
+  RR_cond_fem_concurrent        = 1.0,
   
   # Coital acts
   prob_sex_by_age          = TRUE,
   prob_sex_age_19          = 0.20, # used when prob_sex_by_age == TRUE
-  circum_prob              = 0.4,
-  prop_AI                  = 0.0,
+  prop_AI                  = 0.0,    # CHECK: why this and mean_prop_acts_AI???
   mean_prop_acts_AI        = 0.0,
-  max_age_sex	             = 55,  # From Steve's code, not John's : check on this carefully
-  mean_sex_acts_day        = 0.2, # From Steve's code, not John's : check on this carefully
+  max_age_sex	             = 55,  # From Steve's code, not John's : CHECK on this carefully
+  mean_sex_acts_day        = 0.2, # From Steve's code, not John's : CHECK on this carefully
   
   # Network : to finish
-  #nw_coef_form = c(-Inf, -Inf),
-  relation_dur = 1000,
-  nw_form_terms = "~edges+absdiff('sqrt_age') + offset(nodematch('role',diff=TRUE, keep=1:2))",
-  target_stats = c(initial_pop*meandeg/2, mean_sqrt_age_diff*initial_pop*meandeg/2),
-  
+  relation_dur             = 1000,
+  nw_form_terms            = "~edges+absdiff('sqrt_age') + offset(nodematch('sex', diff=FALSE))",
+  target_stats             = c(initial_pop*meandeg/2, mean_sqrt_age_diff*initial_pop*meandeg/2),
+  nw_coef_form                = -Inf,
   # Testing: to finish
-  
-  mean_test_interval_male  = 1,
 
+  mean_test_interval_male  = 365,    # CHECK with Kathryn for reasonable value?
+  mean_test_interval_female = 365,   # CHECK with Kathryn for reasonable value?
+  mean_test_interval_under25 = 365,  # CHECK with John what this is?
+  test_result_delay = 30,            # Amount of time that it takes to get test results. # CHECK with John
   
   # Treatment: to finish
 
@@ -88,10 +93,13 @@ param_list <- list(
   #prob_care= 1.0,
   #yearly_incr_tx  = 0.0, #  increase in # being treated after the TasP campaign
   
-  
   #testing params
-  testing_model            = "interval"
+  testing_model            = "interval",
 
+  # misc
+  circum_prob              = 0.4
+  
+  
 #  "diagnosis.FUN"      = social_testing_diagnosis_module, #change from PrEP sims
 #  "treatment.FUN"      = social_treatment_module_john_v3,
 
