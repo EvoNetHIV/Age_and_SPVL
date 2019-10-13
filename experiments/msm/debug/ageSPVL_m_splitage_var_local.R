69357f06238f4320a3a7fc2e5d5a510fbdf87fd9
Steven M. Goodreau <goodreau@uw.edu>
2019-10-12 18:39:35 -0700

    Adding to code used for testing how the runs with split age are working.

--- experiments/msm/debug/ageSPVL_m_splitage_var_local.R
+++ experiments/msm/debug/ageSPVL_m_splitage_var_local.R
@@ -1,4 +1,4 @@
-
+    
 #library(devtools)
 #remotes::install_github("https://github.com/chad-klumb/network", ref="tergmLiteterms")
 #remotes::install_github("https://github.com/statnet/tergmLite", ref="tergmLiteterms")
@@ -45,8 +45,8 @@ modules <- c(
 ii <- 1
 
   initial_pop = 10000
-  mean_sqrt_age_diff = 0.4
-  meandeg = 2
+  mean_sqrt_age_diff = 1.2
+  meandeg = 0.66
   min_age = 18
   max_age = 75
   
@@ -64,21 +64,24 @@ ii <- 1
     max_age_sex	= 55,             
     #relation_dur = 1000,          
     susceptibility_var = 0.00,
-    n_steps = 365*1,
+    n_steps = 365*10,
     
     initial_pop = initial_pop,
     initial_infected = 0.1*initial_pop,
 
-    ##### %<25 = 0.187, 0.188, 0.184, 0.193, 0.185, 0.185, 0.186, 0.181, 0.186, 0.182 --- mean = 0.186
+    ##### %<25 = 0.187, 0.188, 0.184, 0.193, 0.185, 0.185, 0.186, 0.181, 0.186, 0.182 
+    ##### --- mean = 0.186
         
-    nw_form_terms = "~edges + nodemix(~age>25) + absdiff('sqrt_age') + offset(nodematch('role',diff=TRUE, keep=1:2))",
+    nw_form_terms = "~edges + nodemix(~age>25, levels2=2:3) + absdiff('sqrt_age') + 
+                        offset(nodematch('role', diff=TRUE, keep=1:2))",
+    
     target_stats = c(initial_pop*meandeg/2,
                      #initial_pop*meandeg*(max_age-25)/(max_age-min_age),
-                     initial_pop*meandeg*0.186*0.814*0.2, initial_pop*meandeg*0.814*0.814*0.8,
+                     initial_pop*meandeg*0.186*0.814*0.2/2, initial_pop*meandeg*0.814*0.814*0.8/2,
                      mean_sqrt_age_diff*initial_pop*meandeg/2),
 
         nw_coef_form = c(-Inf, -Inf),
-    dissolution = "~offset(edges) + offset(nodemix(~age>25))",
+    dissolution = "~offset(edges) + offset(nodemix(~age>25, levels2=2:3))",
     relation_dur = c(50, 316, 2000),
     
     age_dist_new_adds = "min_age",
@@ -111,7 +114,7 @@ ii <- 1
   
   model_name = model_names[ii] 
   evoparams <- do.call(evonet_setup,param_list)
-  evoparams$dissolution <- "~offset(edges) + offset(nodemix(~age>25))"
+  #evoparams$dissolution <- "~offset(edges) + offset(nodemix(~age>25))"
   nw <- nw_setup(evoparams)
   evomodel <- evorun(modules,evoparams,nw)
   assign(model_name,evomodel)
@@ -149,14 +152,17 @@ plot(ff[,1],ff[,2],xlab="age",ylab="age")
 
 # dissolution probabilities by type
 
-ix=which(aa$age<25)
-i1=which((dd$tail %in% ix) + (dd$head %in% ix) == 2)
-i2=which((dd$tail %in% ix) + (dd$head %in% ix) == 1)
-i3=which((dd$tail %in% ix) + (dd$head %in% ix) == 0)
+n_steps = 365*10
 
-plot(dd$onset[i1], pmin(dd$terminus[i1],n_steps+1))
-plot(dd$onset[i2], pmin(dd$terminus[i2],n_steps+1))
-plot(dd$onset[i3], pmin(dd$terminus[i3],n_steps+1))
+ix=which(aa$age<25)
+ix2=which(aa$age>35)
+i1=which((dd$tail%in%ix)&(dd$head%in%ix))
+i2=which( ((dd$tail%in%ix)&(dd$head %in%ix2)) | ((dd$tail%in%ix2)&(dd$head %in%ix)) )
+i3=which((dd$tail%in%ix2)&(dd$head %in% ix2))
+
+plot(pmax(0,dd$onset[i1]), pmin(dd$terminus[i1],n_steps+1))
+plot(pmax(0,dd$onset[i2]), pmin(dd$terminus[i2],n_steps+1))
+plot(pmax(0,dd$onset[i3]), pmin(dd$terminus[i3],n_steps+1))
 
 days_no_diss <- pmin(dd$terminus,n_steps+1) - pmax(dd$onset,0) - 1
 days_diss <- ifelse(dd$terminus==Inf, 0, 1)
@@ -168,3 +174,59 @@ dissprob.oo <- sum(days_diss[i3]) / sum((days_no_diss[i3] + days_diss[i3]))
 1/dissprob.yy
 1/dissprob.yo
 1/dissprob.oo
+
+plot(pmax(0,dd$onset[1:5000]), pmin(dd$terminus[1:5000],n_steps+1))
+
+finalnet <- network.collapse(evomodel$nw[[1]], at=3651)
+summary(finalnet~nodemix(~age>25))
+sum((finalnet%v%'age')>25)
+sum((finalnet%v%'age')<=25)
+
+summary(finalnet~nodemix(~age>25))
+nolder <- sum((finalnet%v%'age')>25)
+nyounger <- sum((finalnet%v%'age')<=25)
+
+
+plot(summary(finalnet~nodemix(~age>25)) )
+     
+plot(summary(finalnet~nodemix(~age>25)) / 
+  c(choose(nyounger,2), nyounger*nolder, choose(nolder,2)))
+
+aa$age[dd$tail[dd$terminus.censored==TRUE]]
+
+
+
+######################
+
+
+netd <- evomodel$nw[[1]]   # cum 222636 edges  (perhaps some with > 1 spell)
+
+spells  <- get.edge.activity(netd, as.spellList = TRUE,
+                                   active.default = TRUE)     #228984
+
+spells2 <- get.edge.activity(netd, as.spellList = TRUE,
+                                   active.default = FALSE)    #228255  ??
+
+spells3 <- get.edge.activity(netd, as.spellList = FALSE,
+                                   active.default = TRUE)     #222636
+
+
+spells4 <- get.edge.activity(netd, as.spellList = FALSE,
+                                   active.default = FALSE)   
+
+length(spells3)                                                      #222636
+sum(sapply(1:length(spells3), function(x) is.null(spells3[[x]])))    #     0
+table(sapply(1:length(spells3), function(x) nrow(spells3[[x]])))     
+#     1      2      3      4      5 
+#216467   5997    167      3      2 
+
+length(spells4)                                                      #222636
+sum(sapply(1:length(spells4), function(x) is.null(spells4[[x]])))    #   729
+table(sapply(1:length(spells4), function(x) nrow(spells4[[x]])))     
+
+
+
+popatts <- evomodel$pop[[1]]
+lm(log(popatts$SetPoint[popatts$Time_Inf>0],10)~
+        popatts$age_infection[popatts$Time_Inf>0])$coef[[2]]
+
