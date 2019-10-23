d4a5f371c1b2dd412e7ac32636a3d5cb1ea17f6b
Steven M. Goodreau <goodreau@uw.edu>
2019-10-23 09:58:31 -0700

    temp code to check local runs

---
+++ experiments/msm/debug/debug post run code.R
@@ -0,0 +1,22 @@
+
+i <- 205
+obj <- ageSPVL_m205_testa
+
+
+popatts[[i]] <- obj$pop
+popsumm[[i]] <- obj$popsumm
+agecoef.list[[i]] <- iSPVL.list[[i]] <- ageinf.list[[i]] <- prev.list[[i]] <-
+numinc.list[[i]] <- agematch.list[[i]] <- meanageinf.list[[i]] <- vector()
+for (j in 1:length(popatts[[i]])) {
+agecoef.list[[i]][j] <- lm(log(popatts[[i]][[j]]$SetPoint[popatts[[i]][[j]]$Time_Inf>0],10)~
+popatts[[i]][[j]]$age_infection[popatts[[i]][[j]]$Time_Inf>0])$coef[[2]]
+iSPVL.list[[i]][j] <- mean(log10(popatts[[i]][[j]]$SetPoint[popatts[[i]][[j]]$Time_Inf>0]),na.rm=TRUE)
+ageinf.list[[i]][j] <- mean(popatts[[i]][[j]]$age_infection,na.rm=TRUE)
+prev.list[[i]][j] <- tail(popsumm[[i]][[j]]$prevalence,1)
+numinc.list[[i]][j] <- sum(popatts[[i]][[j]]$Time_Inf>0, na.rm=TRUE)
+agematch.list[[i]][j] <- obj$nwparam[[1]]$coef.form['absdiff.sqrt_age']
+meanageinf.list[[i]][j] <- mean(popatts[[i]][[j]]$age_infection[popatts[[i]][[j]]$Time_Inf>0], na.rm=TRUE)
+}
+boxplot(agecoef.list)
+abline(h=0)
+
