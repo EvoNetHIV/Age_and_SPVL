3d5653c0af60ac9e5f54b247bee4cd6e076ab16f
Steven M. Goodreau <goodreau@uw.edu>
2019-11-20 14:00:52 -0800

    adds small edits to post run checking code

--- experiments/msm/debug/debug post run code.R
+++ experiments/msm/debug/debug post run code.R
@@ -1,6 +1,8 @@
+#source("experiments/msm/debug/ageSPVL_m211_local.R"); source("experiments/msm/debug/ageSPVL_m212_local.R"); source("experiments/msm/debug/ageSPVL_m213_local.R"); source("experiments/msm/debug/ageSPVL_m214_local.R");
 
-i <- 209
-obj <- ageSPVL_m209_testa
+i <- 219
+runname <- paste('ageSPVL_m', i, '_testa', sep='')
+obj <- get(runname)
 
 
 popatts[[i]] <- obj$pop
@@ -8,18 +10,23 @@ popsumm[[i]] <- obj$popsumm
 agecoef.list[[i]] <- iSPVL.list[[i]] <- ageinf.list[[i]] <- prev.list[[i]] <-
 numinc.list[[i]] <- agematch.list[[i]] <- meanageinf.list[[i]] <- vector()
 for (j in 1:length(popatts[[i]])) {
-agecoef.list[[i]][j] <- lm(log(popatts[[i]][[j]]$SetPoint[popatts[[i]][[j]]$Time_Inf>0],10)~
-popatts[[i]][[j]]$age_infection[popatts[[i]][[j]]$Time_Inf>0])$coef[[2]]
-iSPVL.list[[i]][j] <- mean(log10(popatts[[i]][[j]]$SetPoint[popatts[[i]][[j]]$Time_Inf>0]),na.rm=TRUE)
-ageinf.list[[i]][j] <- mean(popatts[[i]][[j]]$age_infection,na.rm=TRUE)
-prev.list[[i]][j] <- tail(popsumm[[i]][[j]]$prevalence,1)
-numinc.list[[i]][j] <- sum(popatts[[i]][[j]]$Time_Inf>0, na.rm=TRUE)
-agematch.list[[i]][j] <- obj$nwparam[[1]]$coef.form['absdiff.sqrt_age']
-meanageinf.list[[i]][j] <- mean(popatts[[i]][[j]]$age_infection[popatts[[i]][[j]]$Time_Inf>0], na.rm=TRUE)
-}
-boxplot(agecoef.list,xlim=c(201,210))
+  agecoef.list[[i]][j] <- lm(log(popatts[[i]][[j]]$SetPoint[popatts[[i]][[j]]$Time_Inf>0],10)~
+  popatts[[i]][[j]]$age_infection[popatts[[i]][[j]]$Time_Inf>0])$coef[[2]]
+  iSPVL.list[[i]][j] <- mean(log10(popatts[[i]][[j]]$SetPoint[popatts[[i]][[j]]$Time_Inf>0]),na.rm=TRUE)
+  ageinf.list[[i]][j] <- mean(popatts[[i]][[j]]$age_infection,na.rm=TRUE)
+  prev.list[[i]][j] <- tail(popsumm[[i]][[j]]$prevalence,1)
+  numinc.list[[i]][j] <- sum(popatts[[i]][[j]]$Time_Inf>0, na.rm=TRUE)
+  agematch.list[[i]][j] <- obj$nwparam[[1]]$coef.form['absdiff.sqrt_age']
+  meanageinf.list[[i]][j] <- mean(popatts[[i]][[j]]$age_infection[popatts[[i]][[j]]$Time_Inf>0], na.rm=TRUE)
+  }
+
+boxplot(prev.list,xlim=c(201,220))
+abline(h=0)
+
+boxplot(agecoef.list,xlim=c(201,220))
 abline(h=0)
 
 ################################ In debug mode
 #plot(dat$pop$age[dat$discord_coital_df$inf_id], dat$pop$age[dat$discord_coital_df$sus_id])
+evoplot(obj, name=runname)
 
